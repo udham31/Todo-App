dbc38efef3d16478354e30aa299dc703052f786e
udham31 <116259376+udham31@users.noreply.github.com>
2025-01-14 12:02:15 +0530

    update in app.js file

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// conenction to mongodb
-mongoose.connect("mongodb://127.0.0.1:27017/todo_list")
-  .then(() => console.log("Connected to MongoDB!"))
-  .catch(err => console.error("MongoDB connection error:", err));
+// Connect to MongoDB
+async function connectToDB() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/todo_list", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit the app if the connection fails
+  }
+}
 
+connectToDB();
 
-
-// middlewares
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
 
-
-// routes
-app.use(require("./routes/index"))
-app.use(require("./routes/todo"))
-
-
-// server configurations....
+// Server configurations
 app.listen(3000, () => console.log("Server started listening on port: 3000"));
