79351d4841eb875f4d4c527e7a8f6c826a3053de
udham31 <116259376+udham31@users.noreply.github.com>
2025-01-14 11:44:54 +0530

    First commit

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const app = express();
 
 // conenction to mongodb
-mongoose.connect("mongodb://127.0.0.1:27017/todo_list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/todo_list")
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 
 
 // middlewares
